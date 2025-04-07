3091c52630242454eebc0233348563e6f25b1afc
Hamzzzzzi <dlakgjl55@naver.com>
2025-04-07 22:36:12 +0900

    [BOJ_1343]폴리오미노/박현지

--- HyunJi0913/week_01/문자열/BOJ_폴리오미노.java
+++ HyunJi0913/week_01/문자열/BOJ_폴리오미노.java
@@ -1,6 +1,6 @@
 package week_01.문자열;
 import java.util.*;
-public class PGS_폴리오미노 {
+public class BOJ_폴리오미노 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
