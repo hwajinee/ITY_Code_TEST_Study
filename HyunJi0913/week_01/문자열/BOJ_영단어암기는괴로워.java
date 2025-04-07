dc8a4fdce1e14d1928bcfbd05e808a4d4a121a6b
Hamzzzzzi <dlakgjl55@naver.com>
2025-04-07 22:35:15 +0900

    [BOJ-20920]영단어암기는괴로워/박현지

--- HyunJi0913/week_01/문자열/BOJ_영단어암기는괴로워.java
+++ HyunJi0913/week_01/문자열/BOJ_영단어암기는괴로워.java
@@ -1,6 +1,6 @@
 package week_01.문자열;
 import java.util.*;
-public class PGS_영단어암기는괴로워 {
+public class BOJ_영단어암기는괴로워 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
