9d4d06880370ed2c4142daa2f5102a7a988b25a5
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-15 13:18:29 +0900

    Update BOJ_1764.java

--- sojsnake/week_02/set/BOJ_1764.java
+++ sojsnake/week_02/set/BOJ_1764.java
@@ -9,17 +9,24 @@ public class BOJ_1764 {
         Set<String> set = new HashSet<>();
         List<String> list = new ArrayList<>();
 
+
+        // 1번 for문: 듣도 못한 사람 n명 set에 넣기
         for(int i=0; i<n; i++){
             String a = sc.nextLine();
             set.add(a);
         }
+
+        // 2번 for문: 겹치는 사람만 list에 넣기
         for(int i=0; i<m; i++){
             String b = sc.nextLine();
             if(set.contains(b)){
                 list.add(b);
             }
         }
+
+        // list 사전순으로 정렬
         Collections.sort(list);
+        
         System.out.println(list.size());
         for(String s : list){
             System.out.println(s);
