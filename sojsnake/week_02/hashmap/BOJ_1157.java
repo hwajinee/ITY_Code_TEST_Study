76b3c7b4ed2675e130902eca7afdd16e4a94b234
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-15 11:22:27 +0900

    Update BOJ_1157.java

--- sojsnake/week_02/hashmap/BOJ_1157.java
+++ sojsnake/week_02/hashmap/BOJ_1157.java
@@ -36,3 +36,48 @@ public class BOJ_1157 {
             System.out.print((char)(max_index + 'A'));
     }
 }
+
+
+
+// ------------------------------------------------------------------------------------------
+// map을 활용한 풀이법
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().toUpperCase();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        // 1번 for문: 문자 빈도수 세기
+        // getOrDefault(Object key, V DefaultValue): key와 매핑된 값 없을 때 default 값을 반환
+        // 문자 c와 매핑된 value있으면 그 value+1, 없으면 0으로 생각하고 +1 해서 put
+        for (char c : input.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int max = 0;
+        char result = '?';
+
+        // 2번 for문: map 전체 돌면서, 가장 많이 등장한 문자 찾기
+        // key, value 값 둘 다 필요하므로 Map.Entry 사용
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int value = entry.getValue();
+            char key = entry.getKey();
+
+            if (value > max) {
+                max = value;
+                result = key;
+            } 
+            // 같은 빈도 수 가진 알파벳 있으면 ? 출력
+            else if (value == max) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+    }
+}
