8342f89cca35b214c936964fa8e20c83fee374d5
Hamzzzzzi <dlakgjl55@naver.com>
2025-04-21 23:23:33 +0900

    [BOJ-1158] 요세푸스 문제

--- HyunJi0913/week_03/Queue/BOJ_queuestack.java
+++ HyunJi0913/week_03/Queue/BOJ_queuestack.java
@@ -2,4 +2,31 @@ package week_03.Queue;
 import java.util.*;
 public class BOJ_queuestack {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // N
+        int k = sc.nextInt(); // K
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (!queue.isEmpty()) {
+            // K-1번까지는 맨 앞에서 꺼내서 뒤로 보내기
+            for (int i = 0; i < k - 1; i++) {
+                queue.offer(queue.poll());
+            }
+
+            // K번째는 제거
+            sb.append(queue.poll());
+            if (!queue.isEmpty()) sb.append(", ");
+        }
+
+        sb.append(">");
+        System.out.println(sb.toString());
+    }
 }
--- HyunJi0913/week_03/Queue/BOJ_요소푸스문제0.java
+++
@@ -1,4 +0,0 @@
-package week_03.Queue;
-
-public class BOJ_요소푸스문제0 {
-}
