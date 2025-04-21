80532304fb6fafb02fa2a1bba9934f7f77d3a118
Hamzzzzzi <dlakgjl55@naver.com>
2025-04-21 23:11:46 +0900

    [BOJ-12605] 단어순서 뒤집기

--- HyunJi0913/week_03/Stack/BOJ_단어순서뒤집기.java
+++ HyunJi0913/week_03/Stack/BOJ_단어순서뒤집기.java
@@ -1,4 +1,24 @@
 package week_03.Stack;
-
+import java.util.*;
 public class BOJ_단어순서뒤집기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());  // 테스트 케이스 수
+
+        for (int i = 1; i <= N; i++) {
+            String line = sc.nextLine();         // 한 줄 입력 받기
+            String[] words = line.split(" ");    // 단어 단위로 자르기
+
+            // 단어 순서 뒤집기
+            StringBuilder sb = new StringBuilder();
+            sb.append("Case #").append(i).append(": ");
+
+            for (int j = words.length - 1; j >= 0; j--) {
+                sb.append(words[j]);
+                if (j > 0) sb.append(" ");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
 }
