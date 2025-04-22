3b461a1cbfba75103e88ecf431925f3bfe0129f3
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-22 16:24:23 +0900

    Update BOJ_1874.java

--- sojsnake/week_03/stack/BOJ_1874.java
+++ sojsnake/week_03/stack/BOJ_1874.java
@@ -10,21 +10,24 @@ public class BOJ_1874 {
         Stack<Integer> stack = new Stack<>();
         List<Character> list = new ArrayList<>();
 
-        int nextNum = 1; //스택에 들어갈 수(1부터 n까지 증가)
+        int nextNum = 1; //스택에 들어갈 수(1부터 n까지 증가시키면서 출력해야 할 값 찾기)
         for(int i=0; i<n; i++){
             int num = sc.nextInt();
 
-            //
+            //출력해야 할 값보다 현재 값이 작거나 같으면,
             while(nextNum <= num){
+                //스택에 현재 값 넣고, 1 증가
                 stack.push(nextNum);
                 list.add('+');
                 nextNum++;
             }
-            //맨 위의 수와 비교. 같으면 뽑기
+            //맨 위의 수와 출력해야 할 값 비교. 같으면 뽑기
             if(stack.peek() == num) {
                 stack.pop();
                 list.add('-');
-            }else{
+            }
+            //같지 않으면, 연산 불가능한 경우
+            else{
                 System.out.println("NO");
                 return;
             }
