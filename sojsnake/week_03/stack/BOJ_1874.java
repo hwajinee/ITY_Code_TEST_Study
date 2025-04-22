83d346f874d446d0bbd2977e24431bd6cbfd399e
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-22 09:38:00 +0900

    Update BOJ_1874.java

--- sojsnake/week_03/stack/BOJ_1874.java
+++ sojsnake/week_03/stack/BOJ_1874.java
@@ -10,15 +10,17 @@ public class BOJ_1874 {
         Stack<Integer> stack = new Stack<>();
         List<Character> list = new ArrayList<>();
 
-        int nextNum = 1;
+        int nextNum = 1; //스택에 들어갈 수(1부터 n까지 증가)
         for(int i=0; i<n; i++){
             int num = sc.nextInt();
 
+            //
             while(nextNum <= num){
                 stack.push(nextNum);
                 list.add('+');
                 nextNum++;
             }
+            //맨 위의 수와 비교. 같으면 뽑기
             if(stack.peek() == num) {
                 stack.pop();
                 list.add('-');
