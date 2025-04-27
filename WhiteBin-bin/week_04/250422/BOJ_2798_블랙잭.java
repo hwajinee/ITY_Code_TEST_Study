961a6909c74fa0eb3cc7ea65ed0e6dd1214f609d
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-04-27 22:14:53 +0900

    [BOJ-2798] 블랙잭 / 백현빈

---
+++ WhiteBin-bin/week_04/250422/BOJ_2798_블랙잭.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_2798_블랙잭 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] cards = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= m && sum > answer) {
+                        answer = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
