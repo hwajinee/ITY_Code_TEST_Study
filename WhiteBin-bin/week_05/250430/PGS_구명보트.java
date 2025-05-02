236473b5537081474783acce006a4ff39b6df581
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-05-02 16:13:30 +0900

    [PGS] 구명보트 / 백현빈

---
+++ WhiteBin-bin/week_05/250430/PGS_구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int light = 0;
+        int heavy = people.length - 1;
+        int boats = 0;
+
+        while (light <= heavy) {
+            if (people[light] + people[heavy] <= limit) {
+                light++;
+                heavy--;
+            } else {
+                heavy--;
+            }
+            boats++;
+        }
+        return boats;
+    }
+}
