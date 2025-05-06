222dc0f0bd8589c18a421c7410689a4aeec25b65
Hamzzzzzi <dlakgjl55@naver.com>
2025-05-06 23:29:26 +0900

    [BOJ-1253] 좋다 / 박현지

--- HyunJi0913/week_06/TwoPointers/BOJ_좋다.java
+++ HyunJi0913/week_06/TwoPointers/BOJ_좋다.java
@@ -1,4 +1,49 @@
 package week_06.TwoPointers;
-
+import java.util.*;
 public class BOJ_좋다 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);  // 투 포인터를 위해 정렬
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int target = arr[i];
+            int start = 0;
+            int end = n - 1;
+
+            while (start < end) {
+                // 자기 자신은 제외
+                if (start == i) {
+                    start++;
+                    continue;
+                }
+                if (end == i) {
+                    end--;
+                    continue;
+                }
+
+                int sum = arr[start] + arr[end];
+
+                if (sum == target) {
+                    count++;
+                    break;  // 한 번만 만족하면 되므로 break
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
 }
