8c406c58683d04b11c513cf2a0608cc4aeef4e4d
hwajin <hvvxzin99@gmail.com>
2025-05-12 17:52:29 +0900

    [BOJ-17478] 재귀함수가 뭔가요? / 코드 수정

---
+++ hwajinee/week06/PGS_하노이의_탑/Solution.java
@@ -0,0 +1,14 @@
+package week06.PGS_하노이의_탑;
+
+public class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(2);
+
+    }
+}
