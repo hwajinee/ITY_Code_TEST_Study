fa03310eb12431b11bba9593ba1b6c49d41e2b17
Jaejeong Lee <leejae938@gmail.com>
2025-05-23 10:26:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.74 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,40 +6,40 @@ class Solution {
         // 시작점 예약 (0,0,거리 1)
         int[] dr = {1,0,-1,0};
         int[] dc = {0,-1,0,1};
-        
+
         int n = maps.length;
         int m = maps[0].length;
         Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-       
-        
+
         queue.offer(new int[]{0,0,1});
         visited[0][0] = true;
-        
+
         System.out.println(Arrays.deepToString(visited));
         
         while(!queue.isEmpty()) {
-            // 현재 노드 방문
-            int[] cur = queue.poll();
-            int r = cur[0], c= cur[1], dist = cur[2];
-            
-            // if 도착지 도달 시 return dist
-            if (r == n-1 && c == m-1) return dist;
-            
-            // 다음 노드 예약
-            for( int i = 0; i < 4 ; i++) {
-                int nr = r + dr[i], nc = c + dc[i];
-                if (nr>= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc]==1){
-                    if(!visited[nr][nc]){
-                        visited[nr][nc]= true;
-                        queue.offer(new int[]{nr,nc,dist+1});
-                        
+            for(int k = 0; k <queue.size();k++) {
+                // 현재 노드 방문
+                int[] cur = queue.poll();
+                int r = cur[0], c = cur[1], dist = cur[2];
+
+                // if 도착지 도달 시 return dist
+                if (r == n - 1 && c == m - 1) return dist;
+
+                // 다음 노드 예약
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dr[i], nc = c + dc[i];
+
+                    // 배열 범위 넘어가지 않고, 벽이 아니라 길이고, 좌표가 (0,0) 이상일 때
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1) {
+                        if (!visited[nr][nc]) {
+                            visited[nr][nc] = true;
+                            queue.offer(new int[]{nr, nc, dist+1});
+                        }
                     }
                 }
             }
-            
         }
-        
         return -1; // 상대 팀 진영 도착 못 할 때
-    }
+    }   
 }
